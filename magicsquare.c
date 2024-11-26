8af93c370beef127a0372fea6347dc0761659750
rossum99 <108015375+rossum99@users.noreply.github.com>
2024-11-26 19:22:36 +0530

    Update magicsquare.c

--- magicsquare.c
+++ magicsquare.c
@@ -1,42 +1,53 @@
-#include<stdio.h>
-int main()
-{
-	int n,i=0,j=0,k=0,c=1;
-	printf("enter index of square");
-	scanf("%d",&n);
-	int a[n][n];
-	for(i=0;i<n;i++)
-	{for(j=0;j<n;j++)
-	a[i][j]=0;}
-	i=n/2;j=n-1;
-	a[i][j]=1;
-	for(c=2;c<n*n;c++)
-	{
-	i=i-1;
-	j=j+1;
-	if(i==-1)
-	i=n-1;
-	if(j==n)
-	j=0;
-	if(a[i][j]==0)
-	a[i][j]=c;
-	else
-	{
-	i+=1;
-	j-=2;
-	a[i][j]=c;}
-	if(i==-1&&j==n)
-	{
-	i=0;
-	j=n-2;
-	a[i][j]=c;}}
-	
-	for(i=0;i<n;i++)
-	{{for(j=0;j<n;j++)
-	printf("%d  ",a[i][j]);}
-	printf("\n");}
-	return 0;
-	
-	
-	
+#include <stdio.h>
+int main() {
+    int n, i, j, c, newi, newj;
+    printf("Enter index of square: ");
+    scanf("%d", &n);
+    int a[n][n];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            a[i][j] = 0;
+        }
+    }
+
+ 
+    i = n / 2;
+    j = n - 1;
+    a[i][j] = 1;
+
+    for (c = 2; c <= n * n; c++) {
+
+        newi = i - 1;
+        newj = j + 1;
+
+        if (newi == -1 && newj == n) {
+            newi = 0;
+            newj = n - 2;
+        } else {
+        
+            if (newi < 0) 
+                newi = n - 1;
+            if (newj >= n) 
+                newj = 0;
+        }
+
+        if (a[newi][newj] != 0) {
+            newi = i + 1;
+            newj = j;
+        }
+
+        i = newi;
+        j = newj;
+        a[i][j] = c;
+    }
+
+    printf("Magic Square of size %d:\n", n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%3d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
